9bae5663b1f4086e39f2bc71a15909c92f4aa734
Christina Kashevar <christi_k@mail.ru>
2020-09-26 16:21:33 +0300

    feat: add solution for simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.push(value);
+    return this;
+
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+   if (typeof position != 'number' || Math.floor(position) != position ) {
+      this.chain = [];
+      throw "Error";
+    }
+    this.chain.splice(position-1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+     let finalStr= ''
+     for (let i =0; i< this.chain.length-1; i++) {
+       finalStr += `( ${this.chain[i]} )~~`;
+     }
+     finalStr +=`( ${this.chain[this.chain.length-1]} )`;
+     this.chain = [];
+    return finalStr
   }
 };
 
