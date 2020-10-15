f85d17452dbc10ef2ebb30fb6db6626cc2f8b836
Christina Kashevar <christi_k@mail.ru>
2020-10-15 22:54:06 +0300

    feat: add solution vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,60 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(param = true) {
+      this.param = param;
+  }
+
+  encrypt(phrase, key) {
+    if (phrase === undefined || key === undefined) {
+      throw new CustomError('Not implemented');
+    }
+    let result = [];
+    phrase = phrase.toUpperCase();
+    key = key.toUpperCase();
+    for (let i =0, j =0; i <phrase.length; i++) {
+      let charCode = phrase.charCodeAt(i);
+      if ( charCode >= 65 && charCode <= 90) {
+        let phraseNum = phrase[i].charCodeAt() - 65;
+        let keyIndex =  j% key.length;
+        let keyNum = key[keyIndex].charCodeAt() - 65;
+        let charCodeResult = phraseNum + keyNum + 65;
+        if ( charCodeResult > 90) { charCodeResult -=26}
+        result.push( String.fromCharCode(charCodeResult) );
+        j++
+      } else {
+        result.push( phrase[i] )
+      }
+      
+    }
+    if (!this.param) result.reverse();
+    return result.join('');
+  }
+  
+  decrypt(phrase, key) {
+    if (phrase === undefined || key === undefined) {
+      throw new CustomError('Not implemented');
+    }
+    let result = [];
+    phrase = phrase.toUpperCase();
+    key = key.toUpperCase();
+    for (let i =0, j =0; i <phrase.length; i++) {
+      let charCode = phrase.charCodeAt(i);
+      if ( charCode >= 65 && charCode <= 90) {
+        let phraseNum = phrase[i].charCodeAt() - 65;
+        let keyIndex = j% key.length;
+        let keyNum = key[keyIndex].charCodeAt() - 65;
+        let charCodeResult = phraseNum - keyNum + 65;
+        if ( charCodeResult < 65) { charCodeResult +=26}
+        result.push( String.fromCharCode(charCodeResult) );
+        j++
+      } else {
+        result.push( phrase[i] )
+      }
+      
+    }
+    if (!this.param) result.reverse();
+    return result.join('');
   }
 }
 
