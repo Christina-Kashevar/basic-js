a46d1c1c247f7fbc52919faf08c31b358a55f47e
Christina Kashevar <christi_k@mail.ru>
2020-11-01 20:08:25 +0300

    feat: add recursive-depth solution

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,42 +2,15 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth( arr ) {
-    if ( ! Array.isArray(arr) ) {
-      throw new CustomError('Not implemented');
-    }
-    let counter = 1;
+    let depth = 1;
 
-    // arr.flat()
-    // arr.map( item => {
-    //   if (Array.isArray(item)) {
-    //     return 1 + this.calculateDepth(item)
-    //   } 
-    // })
-    // return 1 + this.calculateDepth(arr.flat(counter));
-
-    function compareArrays (arr, newArr) {
-      if (arr.length !== newArr.length) {
-        return false;
+    for(let i = 0; i < arr.length; i++) {
+      if(Array.isArray(arr[i])) {	
+        let newDepth = this.calculateDepth(arr[i]);
+        if(depth < ++newDepth) depth = newDepth;	
       }
-      for (let i =0; i < arr.length; i++) {
-        if (arr[i] !== newArr[i]) {
-          return false
-        }
-      }
-      return true;
     }
-    // return 1 + this.calculateDepth(arr.flat(counter));
 
-    function recursion (arr) {
-      let oldArr = [].concat(arr);
-      arr = arr.flat();
-      if ( compareArrays(oldArr, arr) ) {
-        return counter;
-      }
-      ++counter;
-      recursion (arr)
-    }
-    recursion (arr)
-    return counter
-   }
+    return depth;
+  }
 };
