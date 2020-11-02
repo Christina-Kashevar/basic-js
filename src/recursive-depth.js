40f2f8db4deb2924dbe0158f617e850e63a6a2b8
Christina Kashevar <christi_k@mail.ru>
2020-11-02 15:42:25 +0300

    feat: add recursive-depth solutin

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,15 +2,13 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth( arr ) {
-    let depth = 1;
-
-    for(let i = 0; i < arr.length; i++) {
-      if(Array.isArray(arr[i])) {	
-        let newDepth = this.calculateDepth(arr[i]);
-        if(depth < ++newDepth) depth = newDepth;	
-      }
+    let counter = 1;
+    let newArr = arr.slice();
+    arr = arr.flat();
+    if (JSON.stringify(newArr)!==JSON.stringify(arr)) {
+      ++counter
+     counter= 1 + this.calculateDepth(arr);
     }
-
-    return depth;
+    return counter;
   }
 };
